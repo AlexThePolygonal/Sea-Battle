3e3102f8d31d6cfeb29aeaf86bef3c058a84f295
AlexThePolygonal <alexthepolygonal@gmail.com>
2023-10-17 20:24:46 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 
 template <class T> using vector = std::vector<T>;
 
+
+
+/*
+
+Compile with c++17
+
+*/
+
 int main() {
     Grid<vector, 4, 3, 2, 1> grid(10,10,0);
     long long sum = 0;
     unsigned iters = 5;
+    auto placement = PlacementGrid(grid);
+    auto shooting = ShootingGrid(grid);
+    PlacementGenerator<decltype(placement)> pl;
+    Shooter<decltype(grid)> sh(10,10);
+    
     for (unsigned i = 0; i < iters; i++) {
-        auto placement = PlacementGrid(grid);
-        auto shooting = ShootingGrid(grid);
-        PlacementGenerator<decltype(placement)> pl;
         pl.set(placement);
         pl.generate();
-        Shooter<decltype(grid)> sh(10,10);
         // std::cout << grid.ascii_print() << '\n';
 
         sh.set(shooting);
